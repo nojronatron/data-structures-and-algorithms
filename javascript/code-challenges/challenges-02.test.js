d17d664eee5e7bc5e4c2e4db21071666418b1472
Jon Rumsey <Jon.Rumsey@gmail.com>
2022-04-12 14:06:25 -0700

    passed challenge 3.

--- javascript/code-challenges/challenges-02.test.js
+++ javascript/code-challenges/challenges-02.test.js
@@ -33,7 +33,7 @@ Write a function named addQuestion that, given an array of strings, uses map to 
 ------------------------------------------------------------------------------------------------ */
 
 const addQuestion = (arr) => {
-  // Solution code here...
+  return arr.map( word => word + '?');
 };
 
 /* ------------------------------------------------------------------------------------------------
