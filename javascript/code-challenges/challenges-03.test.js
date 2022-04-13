cd9b6d9de51f03876de41adf30905feae37bf1e6
Jon Rumsey <Jon.Rumsey@gmail.com>
2022-04-13 14:25:37 -0700

    passed fifth challenge.

--- javascript/code-challenges/challenges-03.test.js
+++ javascript/code-challenges/challenges-03.test.js
@@ -61,7 +61,7 @@ For example, notInFirstArray([1,2,3], [1,2,3,4]) returns [4].
 ------------------------------------------------------------------------------------------------ */
 
 const notInFirstArray = (forbiddenValues, arr) => {
-  // Solution code here...
+  return arr.filter( (fVItem) => !forbiddenValues.includes(fVItem));
 };
 
 /* ------------------------------------------------------------------------------------------------
