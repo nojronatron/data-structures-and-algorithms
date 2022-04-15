30c78666fc2f06090cc90a3172130dc9395fd298
Jon Rumsey <Jon.Rumsey@gmail.com>
2022-04-15 15:13:09 -0700

    completed challenge 1

--- javascript/code-challenges/challenges-05.test.js
+++ javascript/code-challenges/challenges-05.test.js
@@ -12,7 +12,10 @@ Note the space in between first and last names.
 You can assume that neither firstName nor lastName will be blank
 ------------------------------------------------------------------------------------------------ */
 const toLastNames = people => {
-  // Solution code here...
+  let result = people.map(
+    item => `${item.firstName} ${item.lastName}`
+  );
+  return result;
 };
 
 /* ------------------------------------------------------------------------------------------------
