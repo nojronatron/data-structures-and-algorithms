20608f799e8ed860d9bf056dbe28a0532867897c
Jon Rumsey <Jon.Rumsey@gmail.com>
2022-04-14 17:32:20 -0700

    completed challenge 8 and 9 and refactored 9

--- javascript/code-challenges/challenges-04.test.js
+++ javascript/code-challenges/challenges-04.test.js
@@ -141,7 +141,15 @@ For example, [1, 14, 0.2, -281, 54782] is only correctly sorted in that order.
 ------------------------------------------------------------------------------------------------ */
 
 const sortNumbersByLength = (arr) => {
-  // Solution code here...
+  return arr.sort((a,b) => {
+    if (`${a}`.length < `${b}`.length) {
+      return -1;
+    }
+    if (`${a}`.length > `${b}`.length) {
+      return 1;
+    }
+    return 0;
+  });
 };
 
 /*-----------------------------------------------------------------------------------------------
@@ -310,7 +318,7 @@ describe('Testing challenge 8', () => {
   });
 });
 
-xdescribe('Testing challenge 9', () => {
+describe('Testing challenge 9', () => {
   test('It should sort numbers by their length', () => {
     expect(sortNumbersByLength([10, 2.8, 1, -47.75])).toStrictEqual([1, 10, 2.8, -47.75]);
     expect(sortNumbersByLength([100, 2.82, 1, -47.75])).toStrictEqual([1, 100, 2.82, -47.75]);
