dc92bc39da42b01b0801fa27ed9678023b0f85d2
Jon Rumsey <Jon.Rumsey@gmail.com>
2022-04-16 11:00:48 -0700

    fixed challenge 7 now all passing.

--- javascript/code-challenges/challenges-01.test.js
+++ javascript/code-challenges/challenges-01.test.js
@@ -52,9 +52,9 @@ CHALLENGE 4
 
 Write a function named `greeting` that takes in a single string and returns the string in all uppercase letters, and followed by an "!".
 
-Then, write a function named `speaker` that takes in an array of strings and a callback function. 
+Then, write a function named `speaker` that takes in an array of strings and a callback function.
 
-Use `forEach` to build a new array of strings, each string modified by the callback. Return the new array. 
+Use `forEach` to build a new array of strings, each string modified by the callback. Return the new array.
 ------------------------------------------------------------------------------------------------ */
 
 const greeting = (word) => {
@@ -144,14 +144,13 @@ const fizzbuzz = (arr) => {
     if (item % 3 === 0 && item % 5 === 0) {
       result.push('Fizz Buzz');
     } else if (item % 3 === 0) {
-      result.push('fizz');
+      result.push('Fizz');
     } else if (item % 5 === 0) {
-      result.push('buzz');
+      result.push('Buzz');
     } else {
       result.push(item);
     }
   });
-  console.log(result);
   return result;
 };
 
@@ -206,7 +205,7 @@ describe('Testing challenge 6', () => {
   });
 });
 
-xdescribe('Testing challenge 7', () => {
+describe('Testing challenge 7', () => {
   const inputs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
 
   test('It should print out messages or numbers', () => {
