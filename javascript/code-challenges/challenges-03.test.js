e35046f41670e5906f419e9b513e5e053f36c12e
Jon Rumsey <Jon.Rumsey@gmail.com>
2022-04-17 16:19:29 -0700

    completed cc03 challenge 6

--- javascript/code-challenges/challenges-03.test.js
+++ javascript/code-challenges/challenges-03.test.js
@@ -116,7 +116,7 @@ For example, getStatName(snorlaxData.stats, 50) will return ['special-defense', 
 ------------------------------------------------------------------------------------------------ */
 
 const getStatName = (arr, minBaseStat) => {
-  // Solution code here...
+  return arr.filter((statItem) => statItem.baseStat> minBaseStat).stat.name;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -263,7 +263,7 @@ describe('Testing challenge 6', () => {
   });
 });
 
-xdescribe('Testing challenge 7', () => {
+describe('Testing challenge 7', () => {
   test('It should return the name of the stats that exceed that maximum', () => {
     expect(getStatName(snorlaxData.stats, 50)).toStrictEqual([ 'special-defense', 'special-attack' ]);
     expect(getStatName(snorlaxData.stats, 50).length).toStrictEqual(2);
