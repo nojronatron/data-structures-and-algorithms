66ce3ad668fd96257a04886b53390d27c40b7113
Jon Rumsey <Jon.Rumsey@gmail.com>
2022-04-20 15:19:22 -0700

    completed cc8 challeng 2.

--- javascript/code-challenges/challenges-08.test.js
+++ javascript/code-challenges/challenges-08.test.js
@@ -82,7 +82,7 @@ Write a function named containsW that takes in a string. This function should us
 ------------------------------------------------------------------------------------------------ */
 
 const containsW = (str) => {
-  // Solution code here...
+  return /w/.test(str);
 };
 
 /* ------------------------------------------------------------------------------------------------
