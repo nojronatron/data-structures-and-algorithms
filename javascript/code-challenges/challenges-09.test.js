7844bd147cd7651d70a62d2880ccf0aab5d199ad
Jon Rumsey <Jon.Rumsey@gmail.com>
2022-04-23 09:45:44 -0700

    cc09 challenge 3 completed.

--- javascript/code-challenges/challenges-09.test.js
+++ javascript/code-challenges/challenges-09.test.js
@@ -37,7 +37,9 @@ Write a function named checkValues that takes in an object and a value and retur
 ------------------------------------------------------------------------------------------------ */
 
 const checkValues = (obj, value) => {
-  // Solution code here...
+  let result = false;
+  Object.values(obj).forEach(val => result = val === value);
+  return result;
 };
 
 /* ------------------------------------------------------------------------------------------------
