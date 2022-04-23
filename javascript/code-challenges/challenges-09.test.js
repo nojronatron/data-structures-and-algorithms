937dc4ad053c0311815cc7b327bf3cd50ea7abab
Jon Rumsey <Jon.Rumsey@gmail.com>
2022-04-23 11:39:17 -0700

    completed cc09 challeng 4.

--- javascript/code-challenges/challenges-09.test.js
+++ javascript/code-challenges/challenges-09.test.js
@@ -62,7 +62,11 @@ HR has asked you to change the data to make it easier to print so that it looks 
 ------------------------------------------------------------------------------------------------ */
 
 const updateNumbers = (obj) => {
-  // Solution code here...
+  let result = [];
+  for(let property in obj) {
+    result.push(`${property}: ${obj[property]}`);
+  }
+  return result;
 };
 
 
