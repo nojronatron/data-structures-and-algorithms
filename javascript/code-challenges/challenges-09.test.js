0f6933b16171e36750ff20a73a2861f302f8d40e
Jon Rumsey <Jon.Rumsey@gmail.com>
2022-04-23 09:16:21 -0700

    cc09 challeng 2 completed.

--- javascript/code-challenges/challenges-09.test.js
+++ javascript/code-challenges/challenges-09.test.js
@@ -25,7 +25,7 @@ const courseInfo = { name: 'Code 301', duration: { dayTrack: '4 weeks', eveningT
 };
 
 const getCourseKeys = (obj) => {
-  // Solution code here...
+  return Object.keys(obj);
 };
 
 /* ------------------------------------------------------------------------------------------------
