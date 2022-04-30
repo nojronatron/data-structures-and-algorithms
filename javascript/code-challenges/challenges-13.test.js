167b549d0746d7c91a734f58121f4be34e87608b
Jon Rumsey <jon.rumsey@gmail.com>
2022-04-30 15:44:27 -0700

    completed cc13 challenge 7

--- javascript/code-challenges/challenges-13.test.js
+++ javascript/code-challenges/challenges-13.test.js
@@ -108,7 +108,11 @@ Write a function named findAnything that takes in an array of strings, along wit
 ------------------------------------------------------------------------------------------------ */
 
 const findAnything = (arr, target) => {
-  // Solution code here...
+  let result = [];
+  arr.forEach(item => {
+    item.includes(target) && result.push(item);
+  });
+  return result;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -248,7 +252,7 @@ describe('Testing challenge 6', () => {
   });
 });
 
-xdescribe('Testing challenge 7', () => {
+describe('Testing challenge 7', () => {
   test('It should find all the strings that contain a given string', () => {
     const words = ['things', 'apple (:)', ':)banana', 'missing that thing', 'cant:)aloupe'];
 
