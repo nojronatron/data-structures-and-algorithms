14a943b815c8c5452bd6cccc5467085368f5c579
Jon Rumsey <jon.rumsey@gmail.com>
2022-04-30 15:59:53 -0700

    completed cc13 challenge 8

--- javascript/code-challenges/challenges-13.test.js
+++ javascript/code-challenges/challenges-13.test.js
@@ -122,7 +122,9 @@ Write a function named findEvery that takes in an array of strings, along with a
 ------------------------------------------------------------------------------------------------ */
 
 const findEvery = (arr, target) => {
-  // Solution code here...
+  return arr.reduce((previousValue, currentValue) => {
+    return currentValue.includes(target) && previousValue ? true : false;
+  }, true);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -261,7 +263,7 @@ describe('Testing challenge 7', () => {
   });
 });
 
-xdescribe('Testing challenge 8', () => {
+describe('Testing challenge 8', () => {
   test('It should determine whether all the strings contain a given string', () => {
     const words = ['things', 'apple pie (:)', ':)banana pie', 'missing that thing', 'cant:)aloupe is tasty'];
 
