6eb099114eaee9e58128537d68e228200f329dbb
Jon Rumsey <Jon.Rumsey@gmail.com>
2022-05-20 16:40:33 -0700

    framed in required methods

--- java/datastructures/lib/src/main/java/datastructures/linkedlist/LinkedList.java
+++ java/datastructures/lib/src/main/java/datastructures/linkedlist/LinkedList.java
@@ -7,11 +7,20 @@ public class LinkedList
     head = new LinkedListNode(null);
   }
   public void insert(LinkedListNode value) {
+    // TODO: implement
+    // adds a new node with the value to the head of the list with an O(1) time performance
+  }
+  public Boolean includes(Object value) {
+    // TODO: implement
+    // indicates whether that value exists as a Node's value somewhere within the list.
 
+    return false;
   }
   @Override
   public String toString()
     {
-        return "null";  // TODO: implement me
+      //  TODO: implement
+      // must iterate through existing Nodes and display their values as a single string
+      return "";
     }
 }
