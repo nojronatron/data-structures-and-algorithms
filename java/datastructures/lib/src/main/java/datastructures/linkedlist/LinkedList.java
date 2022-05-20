0e2bfb86754390af9a037fdd81500d486be45538
Jon Rumsey <Jon.Rumsey@gmail.com>
2022-05-20 16:28:08 -0700

    updated LL Node next prop set to null default

--- java/datastructures/lib/src/main/java/datastructures/linkedlist/LinkedList.java
+++ java/datastructures/lib/src/main/java/datastructures/linkedlist/LinkedList.java
@@ -3,7 +3,12 @@ package datastructures.linkedlist;
 public class LinkedList
 {
   public Node head;
-
+  public LinkedList() {
+    head = new Node(null);
+  }
+  public void insert(Node value) {
+    
+  }
   @Override
   public String toString()
     {
--- java/datastructures/lib/src/main/java/datastructures/linkedlist/Node.java
+++ java/datastructures/lib/src/main/java/datastructures/linkedlist/Node.java
@@ -2,7 +2,7 @@ package datastructures.linkedlist;
 
 public class Node {
   public Object value;
-  public Node next;
+  public Node next=null;
 
   public Node(Object data) {
     value = data;
