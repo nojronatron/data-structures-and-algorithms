e05f2d45268fe361f99626d39871d85a528c3998
Jon Rumsey <Jon.Rumsey@gmail.com>
2022-05-20 16:52:11 -0700

    implemented LinkedList insert method.

--- java/datastructures/lib/src/main/java/datastructures/linkedlist/LinkedList.java
+++ java/datastructures/lib/src/main/java/datastructures/linkedlist/LinkedList.java
@@ -3,14 +3,28 @@ package datastructures.linkedlist;
 public class LinkedList
 {
   // TODO: Implement try-catch structures to return semantically descriptive errors from within calling method
-  public LinkedListNode head;
+  protected LinkedListNode head;
+  protected int count;
+
   public LinkedList() {
     head = new LinkedListNode(null);
   }
-  public void insert(LinkedListNode value) {
-    // TODO: implement
+
+  public void insert(Object value) {
+    // TODO: test
     // adds a new node with the value to the head of the list with an O(1) time performance
+    LinkedListNode Node = new LinkedListNode(1);
+
+    if (count < 1) {
+      head = Node;
+    } else
+    {
+      Node.next = head; // connect new node to the current HEAD node
+    }
+
+    this.count++; // increment the count
   }
+
   public Boolean includes(Object value) {
     // TODO: implement
     // indicates whether that value exists as a Node's value somewhere within the list.
