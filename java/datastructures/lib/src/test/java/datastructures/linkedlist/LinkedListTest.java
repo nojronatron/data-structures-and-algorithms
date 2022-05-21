a09acc657943a398f6ed6336b6c1349f3e8d2201
Jon Rumsey <Jon.Rumsey@gmail.com>
2022-05-20 19:28:44 -0700

    building out tests

--- java/datastructures/lib/src/test/java/datastructures/linkedlist/LinkedListTest.java
+++ java/datastructures/lib/src/test/java/datastructures/linkedlist/LinkedListTest.java
@@ -6,43 +6,53 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class LinkedListTest
-{
-  @Test void testInstantiatesEmptyLinkedList()
-    {
-      LinkedList sut = new LinkedList();
-      System.out.println(sut);
-      assertTrue(true);
-    }
-  @Test void testCanInsertNodeIntoLinkedList()
-  {
+public class LinkedListTest {
+  @Test
+  void testInstantiatesEmptyLinkedList() {
     LinkedList sut = new LinkedList();
     System.out.println(sut);
     assertTrue(true);
-  }  @Test void testHeadPointsToFirstNodeInLinkedList()
-{
-  LinkedList sut = new LinkedList();
-  System.out.println(sut);
-  assertTrue(true);
-}  @Test void testCanInsertMultipleNodesIntoLinkedList()
-{
-  LinkedList sut = new LinkedList();
-  System.out.println(sut);
-  assertTrue(true);
-}  @Test void testReturnsTrueIfValueFoundInLinkedList()
-{
-  LinkedList sut = new LinkedList();
-  System.out.println(sut);
-  assertTrue(true);
-}  @Test void testReturnFalseWhenNotContainsSearchValue()
-{
-  LinkedList sut = new LinkedList();
-  System.out.println(sut);
-  assertTrue(true);
-}  @Test void testCanReturnCollectionOfValuesInLinkedList()
-{
-  LinkedList sut = new LinkedList();
-  System.out.println(sut);
-  assertTrue(true);
-}
+  }
+
+  @Test
+  void testCanInsertNodeIntoLinkedList() {
+    LinkedList sut = new LinkedList();
+    System.out.println(sut);
+    assertTrue(true);
+  }
+
+  @Test
+  void testHeadPointsToFirstNodeInLinkedList() {
+    LinkedList sut = new LinkedList();
+    System.out.println(sut);
+    assertTrue(true);
+  }
+
+  @Test
+  void testCanInsertMultipleNodesIntoLinkedList() {
+    LinkedList sut = new LinkedList();
+    System.out.println(sut);
+    assertTrue(true);
+  }
+
+  @Test
+  void testReturnsTrueIfValueFoundInLinkedList() {
+    LinkedList sut = new LinkedList();
+    System.out.println(sut);
+    assertTrue(true);
+  }
+
+  @Test
+  void testReturnFalseWhenNotContainsSearchValue() {
+    LinkedList sut = new LinkedList();
+    System.out.println(sut);
+    assertTrue(true);
+  }
+
+  @Test
+  void testCanReturnCollectionOfValuesInLinkedList() {
+    LinkedList sut = new LinkedList();
+    System.out.println(sut);
+    assertTrue(true);
+  }
 }
