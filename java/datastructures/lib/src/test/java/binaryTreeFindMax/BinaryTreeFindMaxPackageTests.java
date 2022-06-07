efe6df0e73ceb7b4e8c2dd5a830d9bf00ffed7ba
Jon Rumsey <Jon.Rumsey@gmail.com>
2022-06-07 15:42:51 -0700

    added test and bulkload function happy path test of findmax required bulk loading a tree. FindMax happypath test now working.

--- java/datastructures/lib/src/test/java/binaryTreeFindMax/BinaryTreeFindMaxPackageTests.java
+++ java/datastructures/lib/src/test/java/binaryTreeFindMax/BinaryTreeFindMaxPackageTests.java
@@ -2,15 +2,24 @@ package binaryTreeFindMax;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class BinaryTreeFindMaxPackageTests {
+
+  static NouveauBinaryTree<Integer> batchTree;
+
+  // the arrays.aslist() suggestion came from https://stackoverflow.com/questions/10477628/arraylist-of-int-array-in-java
+  static ArrayList<Integer> bulkLoadValues15 = new ArrayList<>(Arrays.asList(1, 7, 6, 5, 11, 2, 5, 2, 9, 4));
+  static ArrayList<Integer> bulkLoadValues16 = new ArrayList<>(Arrays.asList(2, 7, 5, 2, 6, 9, 5, 11, 4));
 
   @Test
   void testCanInstantiateAnEmptyTree() {
     NouveauBinaryTree sut = new NouveauBinaryTree();
     assertTrue(sut.isEmpty());
+    assertTrue(sut != null);
   }
 
   @Test
@@ -18,5 +27,28 @@ public class BinaryTreeFindMaxPackageTests {
     NouveauBinaryNode<Integer> rootNode = new NouveauBinaryNode<>(11);
     NouveauBinaryTree sut = new NouveauBinaryTree(rootNode);
     assertEquals(false, sut.isEmpty());
+  }
+
+  @Test
+  void testHappyPath() {
+    // test should find the maxvalue in a tree (see sample input in Code Challenge assignment)
+    int expectedResult = 11; // maxval in bulkLoadValues15 is 11
+    NouveauBinaryTree<NouveauBinaryNode<Integer>> sut = new NouveauBinaryTree<>();
+
+    assertTrue(sut.bulkLoadTree(bulkLoadValues15));
+
+    int actualResult = sut.findMax();
+
+    assertEquals(expectedResult, actualResult, "max val in bulkloadvalues15 should be 11.");
+  }
+
+  @Test
+  void testAnExpectedFailure() {
+    
+  }
+
+  @Test
+  void testEdgeCaseOne() {
+
   }
 }
