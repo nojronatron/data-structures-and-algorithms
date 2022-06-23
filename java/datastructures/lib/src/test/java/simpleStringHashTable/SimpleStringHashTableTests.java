74f0f08f1dc6301f797c214529f70b24f8a04a98
Jon Rumsey <Jon.Rumsey@gmail.com>
2022-06-22 22:47:45 -0700

    started adding tests.

---
+++ java/datastructures/lib/src/test/java/simpleStringHashTable/SimpleStringHashTableTests.java
@@ -0,0 +1,60 @@
+package simpleStringHashTable;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SimpleStringHashTableTests {
+  @Test
+  public void testCanInstantiateSimpleStringHashTableTests() {
+    SimpleStringHashTable sut = new SimpleStringHashTable();
+    assertTrue(sut != null);
+  }
+
+  // RepeatWord method tests
+  public void testRepeatWordReturnsEmptyStringWithEmptyInput() {
+
+  }
+
+  public void testRepeatWordReturnsEmptyStringWithSingleLetterInput() {
+
+  }
+
+  public void testRepeatWordReturnsEmptyStringWithSingleWordInput() {
+
+  }
+
+  public void testRepeatWordReturnsFirstDuplicateWord() {
+
+  }
+
+  public void testRepeatWordReturnsFirstDuplicateWordLargeInput() {
+
+  }
+
+  // Hash method tests
+  public void testHashReturnsValidInteger() {
+
+  }
+
+  public void testHashDoesNotThrowOnEmptyStringInput() {
+
+  }
+
+  public void testHashDoesNotThrowOnLargeString() {
+
+  }
+
+  // SumChar method tests
+  public void testSumCharsDoesNotThrowOnEmptyStringInput() {
+
+  }
+
+  public void testSumCharsReturnsValidLong() {
+
+  }
+
+  public void testSumCharsDoesNotThrowOnLargeString() {
+
+  }
+
+}
