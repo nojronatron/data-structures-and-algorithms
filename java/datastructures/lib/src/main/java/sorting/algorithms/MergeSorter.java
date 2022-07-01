5f75ac7c209b44795ee67d7b9fd9d2fb1117d513
Jon Rumsey <Jon.Rumsey@gmail.com>
2022-07-01 15:13:44 -0700

    refactored types, debugging logic.

--- java/datastructures/lib/src/main/java/sorting/algorithms/MergeSorter.java
+++ java/datastructures/lib/src/main/java/sorting/algorithms/MergeSorter.java
@@ -1,19 +1,21 @@
 package sorting.algorithms;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-public class MergeSorter {
-  public void mergesort(ArrayList<Integer> arr) {
+public abstract class MergeSorter {
+  public static void mergesort(int[] arr) {
 
-    int n = arr.size();
+    int n = arr.length;
 
     if (n > 1) {
 
       int mid = n / 2;
 
-      ArrayList<Integer> left = (ArrayList<Integer>) arr.subList(0, mid - 1);
+      int[] left = Arrays.copyOfRange(arr, 0, mid); //arr.subList(0, mid);
 
-      ArrayList<Integer> right = (ArrayList<Integer>) arr.subList(mid, n - 1);
+      int[] right = Arrays.copyOfRange(arr, mid, arr.length); //arr.subList(mid, n);
 
       mergesort(left);
 
@@ -24,39 +26,62 @@ public class MergeSorter {
 
   }
 
-  public void merge(ArrayList<Integer> left, ArrayList<Integer> right, ArrayList<Integer> arr) {
+  public static void merge(int[] left, int[] right, int[] arr) {
     int i = 0;
     int j = 0;
     int k = 0;
 
-    while (i < left.size() && j < right.size()) {
+    while (i < left.length && j < right.length) {
 
-      if (left.get(i) <= right.get(j)) {
-        arr.set(k, left.get(i));
+      if (left[i] <= right[j]) {
+        // int kTemp = left.get(i);
+        int kTemp = Integer.valueOf(left[i]);
+        arr[k] = kTemp; // arr.set(k, left.get(i));
+        // arr.set(k, kTemp);
+
         i++;
       } else {
-        arr.set(k, right.get(i));
+        // int kTemp = right.get(j);
+        // arr.set(k, right.get(j));
+        int kTemp = Integer.valueOf(left[j]);
+        arr[k] = kTemp; //arr.set(k, kTemp);
         j++;
       }
 
       k++;
     }
 
-    if (i == left.size()) {
+    if (i == left.length) {
       // set remaining entries in arr to remaining values in right
-      // from https://code-maze.com/csharp-merge-sort/
-      k++;
-
-      while (i < left.size()) {
-        i++;
-        arr.set(k, left.get(i));
-      }
-
-      while (j < right.size()) {
-        // set remaining entries in arr to remaining values in left
+      for (int idx=k; idx < arr.length; idx++) {
+        arr[idx] = right[j]; // arr.set(idx, Integer.valueOf(right.get(j))); //right.get(j));
         j++;
-        arr.set(k, right.get(j));
+//        arr.set(idx, rightArrayItemValue);
+      }
+    } else {
+      for (int jdx=k; jdx < arr.length; jdx++) {
+        // arr.set(jdx, Integer.valueOf(left.get(k))); //left.get(k));
+        arr[jdx] = left[i];
+        k++;
+//        arr.set(jdx, leftArrayItemValue);
       }
     }
   }
 }
+
+//    if (i == left.size()) {
+//      // set remaining entries in arr to remaining values in right
+//      // from https://code-maze.com/csharp-merge-sort/
+//      k++;
+//
+//      while (i < left.size()) {
+//        i++;
+//        arr.set(k, left.get(i));
+//      }
+//
+//      while (j < right.size()) {
+//        // set remaining entries in arr to remaining values in left
+//        j++;
+//        arr.set(k, right.get(j));
+//      }
+//    }
