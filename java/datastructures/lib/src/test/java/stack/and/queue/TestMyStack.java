a5ece5a47fd9dc4c01501108cb930e26868ddf9d
Jon Rumsey <Jon.Rumsey@gmail.com>
2022-10-27 13:27:18 -0700

    framed in MyStack test suite.

--- java/datastructures/lib/src/test/java/stack/and/queue/TestMyStack.java
+++ java/datastructures/lib/src/test/java/stack/and/queue/TestMyStack.java
@@ -1,4 +1,45 @@
 package stack.and.queue;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 public class TestMyStack {
+  @Test
+  void test_canInstantiateMyStack() {
+    MyStack<Integer> sut = new MyStack<>();
+    assertNotNull(sut);
+  }
+  @Test
+  void test_canPushOntoStack() {
+
+  }
+  @Test
+  void test_canPushMultipleOntoStack() {
+
+  }
+  @Test
+  void test_canPopOffStack() {
+
+  }
+  @Test
+  void test_canEmptyStackAfterMultiplePops() {
+
+  }
+  @Test
+  void test_canPeekTopItemOnStack() {
+
+  }
+  @Test
+  void test_callingPopOnEmptyStackRaisesException() {
+
+  }
+  @Test
+  void test_callingPeekOnEmptyStackRaisesException() {
+
+  }
+  @Test
+  void test_isEmptyReturnsTrueWhenEmptyFalseWhenHasItems() {
+
+  }
 }
