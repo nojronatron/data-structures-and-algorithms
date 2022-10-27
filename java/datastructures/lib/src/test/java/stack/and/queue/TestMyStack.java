1ebd69bcf090bf42ac61fec92187046330ac539a
Jon Rumsey <Jon.Rumsey@gmail.com>
2022-10-27 15:38:14 -0700

    added tests for all methods, all passing.

--- java/datastructures/lib/src/test/java/stack/and/queue/TestMyStack.java
+++ java/datastructures/lib/src/test/java/stack/and/queue/TestMyStack.java
@@ -2,7 +2,7 @@ package stack.and.queue;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TestMyStack {
   @Test
@@ -10,36 +10,145 @@ public class TestMyStack {
     MyStack<Integer> sut = new MyStack<>();
     assertNotNull(sut);
   }
+
   @Test
   void test_canPushOntoStack() {
-
+    int expectedValue = 11;
+    MyStack<Integer> sut = new MyStack<>();
+    assertTrue(sut.isEmpty());
+    assertTrue(sut.push(expectedValue));
+    assertFalse(sut.isEmpty());
   }
+
   @Test
   void test_canPushMultipleOntoStack() {
+    String expectedFirstValue = "s";
+    String expectedSecondValue = "e";
+    String expectedThirdValue = "y";
 
+    MyStack<String> sut = new MyStack<>();
+
+    assertTrue(sut.isEmpty());
+    assertTrue(sut.push(expectedFirstValue));
+    assertFalse(sut.isEmpty());
+    assertTrue(sut.push(expectedSecondValue));
+    assertFalse(sut.isEmpty());
+    assertTrue(sut.push(expectedThirdValue));
+    assertFalse(sut.isEmpty());
   }
+
   @Test
   void test_canPopOffStack() {
+    String expectedFirstValue = "yes";
 
+    MyStack<String> sut = new MyStack<>();
+
+    assertTrue(sut.isEmpty());
+    assertTrue(sut.push(expectedFirstValue));
+    assertFalse(sut.isEmpty());
+
+    String actualResult = sut.pop();
+    assertTrue(sut.isEmpty());
+    assertEquals(expectedFirstValue, actualResult);
   }
+
   @Test
   void test_canEmptyStackAfterMultiplePops() {
+    String expectedFirstValue = "s";
+    String expectedSecondValue = "e";
+    String expectedThirdValue = "y";
 
+    MyStack<String> sut = new MyStack<>();
+
+    assertTrue(sut.push(expectedFirstValue));
+    assertTrue(sut.push(expectedSecondValue));
+    assertTrue(sut.push(expectedThirdValue));
+
+    assertEquals(expectedThirdValue, sut.pop());
+    assertFalse(sut.isEmpty());
+
+    assertEquals(expectedSecondValue, sut.pop());
+    assertFalse(sut.isEmpty());
+
+    assertEquals(expectedFirstValue, sut.pop());
+    assertTrue(sut.isEmpty());
   }
+
   @Test
   void test_canPeekTopItemOnStack() {
+    String expectedFirstValue = "yes";
+    String expectedSecondValue = "me too";
+    MyStack<String> sut = new MyStack<>();
 
+    assertTrue(sut.isEmpty());
+    assertTrue(sut.push(expectedFirstValue));
+    assertFalse(sut.isEmpty());
+
+    String actualFirstResult = sut.peek();
+    assertEquals(expectedFirstValue, actualFirstResult);
+    assertFalse(sut.isEmpty());
+
+    assertTrue(sut.push(expectedSecondValue));
+    String actualSecondResult = sut.peek();
+    assertEquals(expectedSecondValue, actualSecondResult);
   }
+
   @Test
   void test_callingPopOnEmptyStackRaisesException() {
+    MyStack<String> sut = new MyStack<>();
 
+    assertTrue(sut.isEmpty());
+    assertThrows(NullPointerException.class, () -> {
+      sut.pop();
+    });
+    assertTrue(sut.isEmpty());
   }
+
   @Test
   void test_callingPeekOnEmptyStackRaisesException() {
+    String expectedSingleValue = "1";
+    MyStack<String> sut = new MyStack<>();
 
+    assertTrue(sut.isEmpty());
+    assertThrows(NullPointerException.class, () -> {
+      sut.peek();
+    });
+
+    assertTrue(sut.push(expectedSingleValue));
+    assertDoesNotThrow(() -> {
+      sut.peek();
+    });
   }
-  @Test
-  void test_isEmptyReturnsTrueWhenEmptyFalseWhenHasItems() {
 
+  @Test
+  void test_isEmptyReturnsTrueWhenEmptyAndFalseWhenHasItems() {
+    String expectedFirstValue = "yes";
+
+    MyStack<String> sut = new MyStack<>();
+
+    assertTrue(sut.isEmpty());
+
+    assertTrue(sut.push(expectedFirstValue));
+    assertFalse(sut.isEmpty());
+
+    sut.pop();
+    assertTrue(sut.isEmpty());
+  }
+
+  @Test
+  void test_toStringReturnsCorrectSequence() {
+    String expectedResult = "[y]->[e]->[s]->NULL";
+    String expectedFirstValue = "s";
+    String expectedSecondValue = "e";
+    String expectedThirdValue = "y";
+
+    MyStack<String> sut = new MyStack<>();
+
+    assertTrue(sut.push(expectedFirstValue));
+    assertTrue(sut.push(expectedSecondValue));
+    assertTrue(sut.push(expectedThirdValue));
+
+    String actualResult = sut.toString();
+    assertEquals(expectedResult, actualResult);
   }
 }
