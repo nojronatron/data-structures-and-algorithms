df550726a3c5b5230609fba37186ac4804ba189c
Jon Rumsey <Jon.Rumsey@gmail.com>
2022-10-27 18:36:34 -0700

    built out unit tests suite, all passing.

--- java/datastructures/lib/src/test/java/stack/and/queue/TestBalancedBrackets.java
+++ java/datastructures/lib/src/test/java/stack/and/queue/TestBalancedBrackets.java
@@ -1,4 +1,75 @@
 package stack.and.queue;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class TestBalancedBrackets {
+  @Test
+  void test_PairsOfParensReturnsTrue() {
+    String expectedInput = "()";
+    var sut = new BalancedBrackets();
+    assertTrue(sut.IsBalanced(expectedInput));
+  }
+  @Test
+  void test_PairsOfBracketsReturnsTrue() {
+    String expectedInput = "[]";
+    var sut = new BalancedBrackets();
+    assertTrue(sut.IsBalanced(expectedInput));
+  }
+  @Test
+  void test_PairsOfBracesReturnsTrue() {
+    String expectedInput = "{}";
+    var sut = new BalancedBrackets();
+    assertTrue(sut.IsBalanced(expectedInput));
+  }
+  @Test
+  void test_SingleParenthesisReturnsFalse() {
+    String expectedInput = "(";
+    var sut = new BalancedBrackets();
+    assertFalse(sut.IsBalanced(expectedInput));
+  }
+  @Test
+  void test_SingleParenthesisSurroundedByPairOFBracketsReturnsFalse() {
+    String expectedInput = "[(]";
+    var sut = new BalancedBrackets();
+    assertFalse(sut.IsBalanced(expectedInput));
+  }
+  @Test
+  void test_SingleBraceSurroundedByPairOfBracesReturnsFalse() {
+    String expectedInput = "{{}";
+    var sut = new BalancedBrackets();
+    assertFalse(sut.IsBalanced(expectedInput));
+  }
+  @Test
+  void test_MultiplePairsOfBracketsBracesParenthesesReturnsTrue() {
+    String expectedInput = "[{}[]({({})}[])]";
+    var sut = new BalancedBrackets();
+    assertTrue(sut.IsBalanced(expectedInput));
+  }
+  @Test
+  void test_EmptyInputReturnsFalse() {
+    String expectedInput = "";
+    var sut = new BalancedBrackets();
+    assertFalse(sut.IsBalanced(expectedInput));
+  }
+  @Test
+  void test_OtherInputTypesReturnsFalse() {
+    String expectedInput = "abccbbbabbacddceea";
+    var sut = new BalancedBrackets();
+    assertFalse(sut.IsBalanced(expectedInput));
+  }
+  @Test
+  void test_MixedInputTypesWithoutMatchingPairsReturnsFalse() {
+    String expectedInput = "{abc[cbbba)bbacddceea";
+    var sut = new BalancedBrackets();
+    assertFalse(sut.IsBalanced(expectedInput));
+  }
+  @Test
+  void test_OtherInputTypesWithMatchingPairsReturnsTrue() {
+    String expectedInput = "{a[b(cc)b(bb)a]b{b}{a}c[ddc()ee]a}";
+    var sut = new BalancedBrackets();
+    assertTrue(sut.IsBalanced(expectedInput));
+  }
 }
